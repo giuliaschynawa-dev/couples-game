01617c01806db052fbeef0678b32dd81052fad23
giuliaschynawa-dev <giulia.schynawa@gmail.com>
2025-09-07 22:51:54 +0200

    Add files via upload

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "couples-game",
+  "version": "1.0.0",
+  "description": "A synchronized couple's game.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "socket.io": "^4.7.5"
+  }
+}
